8db70344c9c8445b3e8b386a302937c573b1bca0
tronget <andrejlashkul@gmail.com>
2025-08-02 14:26:34 +0300

    new trim functions

--- strutil/trim.go
+++ strutil/trim.go
@@ -2,10 +2,45 @@ package strutil
 
 import (
 	"regexp"
+	"strings"
 )
 
 var nonAlphanumericRegex = regexp.MustCompile(`^[^a-z0-9]+|[^a-z0-9]+$`)
 
 func TrimSpec(s string) string {
 	return nonAlphanumericRegex.ReplaceAllString(s, "")
+}
+
+func TrimSubstring(s, substr string) string {
+	s = TrimSubLeft(s, substr)
+	s = TrimSubRight(s, substr)
+	return s
+}
+
+func TrimSubLeft(s, substr string) string {
+	if substr == "" {
+		return s
+	}
+	for strings.HasPrefix(s, substr) {
+		s = s[len(substr):]
+	}
+	return s
+}
+
+func TrimSubRight(s, substr string) string {
+	if substr == "" {
+		return s
+	}
+	for strings.HasSuffix(s, substr) {
+		s = s[:len(s)-len(substr)]
+	}
+	return s
+}
+
+func TrimToLength(s string, n int) string {
+	s = strings.TrimSpace(s)
+	if len(s) > n {
+		return s[:n]
+	}
+	return s
 }
